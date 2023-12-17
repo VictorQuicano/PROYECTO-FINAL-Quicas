defe03109387cab463982c54c5fa0aa557fd1f15
Alejandro <alejandroxd25k@gmail.com>
2023-12-17 17:32:31 -0500

    Solucion smell "refact redudant 'await'"

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -4,7 +4,7 @@ const {Image} = require('../models')
 const sidebar = require('../helpers/sidebar')
 
 ctrl.index = async (req , res)=>{
-    const images = await Image.find().sort({timestamp : -1});
+    const images = Image.find().sort({timestamp : -1});
     let viewmodel = {images:[]};
     viewmodel.images = images ;
     viewmodel = await sidebar(viewmodel);
